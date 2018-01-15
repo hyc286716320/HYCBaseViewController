ee477ba6574266b8d2e7220432d33bfa3aed88b3
HuYunchao <286716320@qq.com>
2018-01-15 16:41:55 +0800

    GOGO

--- HYCBaseViewController.podspec
+++ HYCBaseViewController.podspec
@@ -1,13 +1,15 @@
+
 Pod::Spec.new do |s|
+s.name     = 'HYCBaseViewController'
+s.version  = '1.1' 
+s.license  = "MIT"
+s.summary  = 'UIViewController父类,能快速实现UINav的一些设置,非常实用' 
+s.homepage = 'https://github.com/hyc286716320/HYCBaseViewController'
+s.author   = {'HuYunchao' => 'hyc286716320'} 
+s.source   = { :git => 'https://github.com/hyc286716320/HYCBaseViewController.git', :tag =>s.version} 
+s.platform     = :ios 
+s.source_files = 'HYCBaseViewController'
+s.requires_arc = true
 
-  s.name         = "HYCBaseViewController"
-  s.version      = "0.0.1"
-  s.summary      = "Super ViewController"
-  s.description  = "this is Super ViewController"
-  s.homepage     = "https://github.com/hyc286716320/HYCBaseViewController"
-  s.license      = "MIT"
-  s.author             = { "HuYunchao" => "286716320@qq.com" }
-  s.source       = { :git => "https://github.com/hyc286716320/HYCBaseViewController.git", :tag => s.version.to_s }
-  s.source_files  = 'HYCBaseViewController/*'
-
+#s.dependency 'TMUtils', '~> 1.0'
 end
---
+++ HYCBaseViewController/UIBarButtonItem+Badge.h
@@ -0,0 +1,35 @@
+//
+//  UIBarButtonItem+Badge.h
+//  JLCInCircleTransact
+//
+//  Created by hyc on 2017/11/29.
+//  Copyright © 2017年 hyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>  
+
+@interface UIBarButtonItem (Badge)  
+
+@property (strong, nonatomic) UILabel *badge;  
+
+// Badge value to be display  
+@property (nonatomic) NSString *badgeValue;  
+// Badge background color  
+@property (nonatomic) UIColor *badgeBGColor;
+// Badge text color  
+@property (nonatomic) UIColor *badgeTextColor;  
+// Badge font  
+@property (nonatomic) UIFont *badgeFont;  
+// Padding value for the badge  
+@property (nonatomic) CGFloat badgePadding;  
+// Minimum size badge to small  
+@property (nonatomic) CGFloat badgeMinSize;  
+// Values for offseting the badge over the BarButtonItem you picked  
+@property (nonatomic) CGFloat badgeOriginX;  
+@property (nonatomic) CGFloat badgeOriginY;  
+// In case of numbers, remove the badge when reaching zero  
+@property BOOL shouldHideBadgeAtZero;  
+// Badge has a bounce animation when value changes  
+@property BOOL shouldAnimateBadge;  
+
+@end  
